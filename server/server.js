e1b7dd687ad408ede1a435477c15e4afafe53cdd
codemayank <mayankyadav14@gmail.com>
2018-02-27 16:36:20 +0530

    commiting server.js

---
+++ server/server.js
@@ -0,0 +1,14 @@
+
+const path = require("path");
+const express = require("express");
+
+const publicPath = path.join(__dirname, '../public');
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static('publicPath'));
+
+
+app.listen(PORT, function(){
+  console.log('the server is now running on port ' + PORT);
+})
