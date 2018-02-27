49aec815ff7a3a53e22cda1d59a7538843c62b88
codemayank <mayankyadav14@gmail.com>
2018-02-27 18:43:31 +0530

    add index.js

---
+++ public/js/index.js
@@ -0,0 +1,14 @@
+let socket = io();
+//connect is the built in event on server.js
+socket.on('connect', function(){
+  console.log('connected to server');
+})
+
+socket.on('disconnect', function(){
+console.log('disconnected from server');
+})
+
+
+socket.on('message', function(message){
+  console.log('message', message);
+})
